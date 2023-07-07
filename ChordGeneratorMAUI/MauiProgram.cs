846299a58b30a6a390c05472200925cbf43a57fe
ArctechFox <jsteed.arctech@gmail.com>
2023-07-07 15:05:42 -0700

    Added some fonts (that Catt doesn't really like lol), shuffled some stuff around on the Purchase Page.

--- ChordGeneratorMAUI/MauiProgram.cs
+++ ChordGeneratorMAUI/MauiProgram.cs
@@ -2,6 +2,7 @@
 using CommunityToolkit.Maui;
 using Microsoft.Extensions.Logging;
 using Plugin.Maui.Audio;
+using Windows.Media;
 
 namespace ChordGeneratorMAUI;
 
@@ -16,11 +17,25 @@ public static class MauiProgram
             .UseMauiCommunityToolkitMediaElement()
             .ConfigureFonts(fonts =>
 			{
+				// Logo/title font
+				fonts.AddFont("Stark.otf", "Stark");
+
+				// Main sans-serif font
+				fonts.AddFont("Sansation_Bold.ttf", "Sansation Bold");
+				fonts.AddFont("Sansation_Bold_italic.ttf", "Sansation Bold-Italic");
+				fonts.AddFont("Sansation_Italic.ttf", "Sansation Italic");
+				fonts.AddFont("Sansation_Light.ttf", "Sansation Light");
+				fonts.AddFont("Sansation_Light_Italic.ttf", "Sansation Light-Italic");
+				fonts.AddFont("Sansation_Regular.ttf", "Sansation");
+
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+				// Music notation
 				fonts.AddFont("LASSUS.ttf", "Lassus");
 				fonts.AddFont("MusAnalysis.otf", "MusicAnalysis");
 
+				// Icon fonts
 				fonts.AddFont("Font Awesome 5 Brands-Regular-400.otf", "FA Brands");
 				fonts.AddFont("Font Awesome 5 Duotone-Solid-900.otf", "FA Duotone");
 				fonts.AddFont("Font Awesome 5 Pro-Light-300.otf", "FA Pro Light");
